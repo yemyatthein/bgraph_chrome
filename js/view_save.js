f9eddc206504141804d15f13a8de74ef5db3e3ef
YeMyat Thein <yemyatthein@gmail.com>
2016-01-31 22:53:39 +0000

    server side basic

--- js/view_save.js
+++ js/view_save.js
@@ -14,6 +14,36 @@ window.addEventListener('DOMContentLoaded', function(evt) {
         var concept_name = eventPage.bgraph.bg_page.data.concept_name;
         $(".current_concept_title").text(concept_name);
 
+        $("#save_in_cloud").click(function() {
+			var original_data = eventPage.bgraph.bg_page.data;
+			var refined_data  = eventPage.bgraph.bg_page.refined_data;
+
+			var concept_data = {
+				name 		: "Popular Stuffs",
+				description	: "It's about the popular news searching and reading.",
+				tags		: "news gadget juzforlolz",
+				visibility	: "PUBLIC"
+			};
+
+			var form_data = {
+				concept  : JSON.stringify(concept_data),
+				original : JSON.stringify(original_data),
+				refined  : JSON.stringify(refined_data)
+			};
+
+			console.log("DEBUG: Submitting data to cloud.", form_data);
+
+			$.ajax({
+				type: "POST",
+				url: "http://localhost:5000/save",
+				data: form_data,
+				success: function(response) {
+					console.log("RETURNED: ", response);
+				},
+				dataType: "json"
+			});
+        });
+
     });
     
 });
--- server/main.py
+++ server/main.py
@@ -1,6 +1,9 @@
+import json
 from flask import (Flask, request, make_response, jsonify, redirect)
 
+
 app = Flask(__name__)
+
 
 @app.route('/sample')
 def index():
@@ -10,6 +13,17 @@ def index():
 		{'name': 'Mondy', 'age': 7, 'school': 'Darrern Loo Junior'},
 	]}
 	return jsonify(data)
+
+
+@app.route('/save', methods=["POST"])
+def save():
+	data = {
+		'concept': json.loads(request.form['concept']),
+		'original': json.loads(request.form['original']),
+		'refined': json.loads(request.form['refined']),
+	}
+
+	return jsonify({'response': data})
 
 
 if __name__ == '__main__':
