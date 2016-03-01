bddd5b474fea2f0ade46045ff23ff8739fb8a71c
YeMyat Thein <yemyatthein@gmail.com>
2016-03-01 19:31:54 +0000

    before move back to local

--- server/main.py
+++ server/main.py
@@ -99,12 +99,9 @@ def all_concepts():
     return render_template('all_concepts.html', data=data)
 
 
-#if __name__ == '__main__':
-#    app.run()#debug=True, host='0.0.0.0')
-
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port) #app.run(debug=True, host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True, port=port)
 
 
 
